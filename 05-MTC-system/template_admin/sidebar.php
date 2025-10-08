d52b815dec3e461cb884dc5d9e2ebcc65f3f1a1a
Nazwa Nurfadhilla Phitri <112637218+nazwa-np@users.noreply.github.com>
2025-10-08 13:08:06 +0700

    Add files via upload

---
+++ 05-MTC-system/template_admin/sidebar.php
@@ -0,0 +1,43 @@
+<div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Home</div>
+                            <a class="nav-link" href="<?= $main_url ?>halaman_admin.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Monitoring
+                            </a>
+                            <hr class="sidebar-divider"> 
+                            <div class="sb-sidenav-menu-heading">Admin</div>
+                            <a class="nav-link" href="<?= $main_url ?>user/admin_user.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-solid fa-user"></i></div>
+                                User
+                            </a>
+                            <hr class="sidebar-divider"> 
+                            <div class="sb-sidenav-menu-heading">System</div>
+                            <a class="nav-link" href="<?= $main_url ?>sap/entry_sap.php">
+                                <div class="sb-nav-link-icon"><i class="fa-regular fa-pen-to-square"></i></div>
+                                Entry SAP
+                            </a>
+                            <a class="nav-link" href="<?= $main_url ?>update_monitoring/data.php">
+                                <div class="sb-nav-link-icon"><i class="fa-regular fa-pen-to-square"></i></div>
+                                Update Monitoring
+                            </a>
+                            </a>
+                            <a class="nav-link" href="<?= $main_url ?>list_material/list.php">
+                                <div class="sb-nav-link-icon"><i class="fa-regular fa-pen-to-square"></i></div>
+                                List Material
+                            </a>
+                        
+                        
+                        </div>
+                    </div>
+      
+                                
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged In As</div>
+                        <div class="small">Admin</div>
+                    </div>
+                </nav>
+            </div>
